0b93d95d7f17ea8a5c97e2f676d47b2994b3529f
danielgarzonotero <vvd9fd@virginia.edu>
2025-02-21 14:41:56 -0500

    update

--- __init__.py
+++ __init__.py
@@ -1,7 +1,5 @@
 # smiles2peptides/__init__.py
-
 from .main import generating_rdkit_mol
 from .functions.processing_seq import process_sequence
 from .functions.creating_dic import create_dictionary_from_excel
 
-# También puedes agregar otras funciones o clases clave si es necesario
