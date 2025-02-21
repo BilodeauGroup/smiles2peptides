0a5a306a1bc9e66f91537eb8f6890befd443f33a
danielgarzonotero <vvd9fd@virginia.edu>
2025-02-21 15:33:13 -0500

    update

--- setup.py
+++ setup.py
@@ -1,18 +1,20 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="smiles2peptides", 
-    version="0.1",  
-    packages=find_packages(include=["smiles2peptides", "smiles2peptides.*", "data", "functions"]),  # Asegúrate de incluir todos los subdirectorios relevantes
-    install_requires=[  
-        "pandas>=1.3.5",  # Especifica una versión para evitar incompatibilidades
-        "rdkit>=2023.3.2",  # Especifica una versión para evitar incompatibilidades
+    name="smiles2peptides",
+    version="0.1",
+    packages=find_packages(include=["smiles2peptides", "smiles2peptides.*"]),
+    install_requires=[
+        "pandas>=1.3.5",
+        "rdkit>=2023.3.2",
     ],
+    include_package_data=True,  # Importante para incluir archivos no Python
+    package_data={"": ["data/*.xlsx"]},  # Asegura que los archivos de data/ sean incluidos
     author="Daniel Garzon Otero",
     author_email="vvd9fd@virginia.edu",
     description="Un módulo para convertir SMILES a péptidos y análisis de estructuras moleculares",
-    long_description=open('README.md').read() if 'README.md' in locals() else 'Descripción del paquete',  
-    long_description_content_type="text/markdown",  
-    url="https://github.com/danielgarzonotero/smiles2peptides.git",  
-    python_requires='>=3.7.16',  # Asegúrate de que funcione con Python 3.7 o superior
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/danielgarzonotero/smiles2peptides",
+    python_requires=">=3.7.16",
 )
