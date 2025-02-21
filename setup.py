541458c098fbac2435ce3f1607d9f46bbb32ca5a
danielgarzonotero <vvd9fd@virginia.edu>
2025-02-21 14:55:28 -0500

    update

--- setup.py
+++ setup.py
@@ -3,22 +3,23 @@ from setuptools import setup, find_packages
 setup(
     name="smiles2peptides", 
     version="0.1",  
-    packages=find_packages(),  
+    packages=find_packages(),  # Detecta automáticamente las carpetas y archivos Python
     install_requires=[  
-        "pandas",  # Especificando la versión para evitar incompatibilidades
-        "rdkit",  # Igualmente, especifica la versión exacta de RDKit
+        "pandas>=1.3.5",  # Especifica una versión para evitar incompatibilidades
+        "rdkit>=2023.3.2",  # Especifica una versión para evitar incompatibilidades
     ],
     entry_points={
         "console_scripts": [
-            "smiles2peptides=smiles2peptides.main:generating_rdkit_mol",  
+            # Si deseas exponer la funcionalidad como un comando en la terminal
+            # "smiles2peptides=smiles2peptides.main:generating_rdkit_mol",  # Esto sería mejor para la función que definiste
         ],
     },
     author="Daniel Garzon Otero",
     author_email="vvd9fd@virginia.edu",
     description="Un módulo para convertir SMILES a péptidos y análisis de estructuras moleculares",
-    long_description=open('README.md').read(),  
+    long_description=open('README.md').read() if 'README.md' in locals() else 'Descripción del paquete',  
     long_description_content_type="text/markdown",  
     url="https://github.com/danielgarzonotero/smiles2peptides.git",  
-    python_requires='>=3.7.16',  # Asegurarse de que funcione con Python 3.7 o superior
+    python_requires='>=3.7.16',  # Asegúrate de que funcione con Python 3.7 o superior
 )
 
