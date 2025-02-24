2ce0e9b882f5866498ced045901bb65ecd7e9d36
danielgarzonotero <vvd9fd@virginia.edu>
2025-02-24 15:24:34 -0500

    init

--- smiles2peptides/__init__.py
+++ smiles2peptides/__init__.py
@@ -1,6 +1,4 @@
 # Importar funciones o clases principales para facilitar el acceso a través del paquete
 from .main import generating_rdkit_mol
-from .main import create_dictionary_from_excel
-from .main import extract_characters
-from .main import process_sequence
+
 
