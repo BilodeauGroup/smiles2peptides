a6967a06e539895d0d8ed7274b9b66223532c899
danielgarzonotero <vvd9fd@virginia.edu>
2025-02-24 11:17:58 -0500

    SETUP

--- setup.py
+++ setup.py
@@ -1,11 +1,10 @@
 from setuptools import setup, find_packages
 
-# Leer el contenido de README.md si está disponible
 try:
     with open("README.md", "r", encoding="utf-8") as f:
         long_description = f.read()
 except FileNotFoundError:
-    long_description = "Un módulo para convertir SMILES a péptidos y análisis de estructuras moleculares"
+    long_description = "This library is to obtain the RDKit molecule from the amino acid sequence including the non-natural ones present in the library"
 
 setup(
     name="smiles2peptides", 
@@ -15,13 +14,13 @@ setup(
         "pandas>=1.3.5",
         "rdkit>=2023.3.2",
     ],
-    include_package_data=True,  # Asegura que se incluyan archivos no Python
+    include_package_data=True, 
     package_data={
-        "smiles2peptides": ["amino_acid_library.xlsx"],  # Especifica el archivo a incluir
+        "smiles2peptides": ["amino_acid_library.xlsx"], 
     },
     author="Daniel Garzon Otero",
     author_email="vvd9fd@virginia.edu",
-    description="Un módulo para convertir SMILES a péptidos y análisis de estructuras moleculares",
+    description="A module to obtain the RDKit molecule from an amino acid sequence",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/danielgarzonotero/smiles2peptides.git",
